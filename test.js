42d5e7f053ddfdaed604c13f74e3825dc346b38d
D45Hub <denis.thiessen@gmx.de>
2022-02-05 22:33:25 +0100

    Added second addition to addition test

--- test.js
+++ test.js
@@ -10,24 +10,32 @@ const OUTPUT_MODULE_PATH = path.resolve(__dirname + "/src/bf/", 'resultJson.b')
 const EQUALS_MODULE = path.resolve(__dirname + "/src/test/", 'equals.b')
 
 const MODULE_45_PATH = path.resolve(__dirname + "/src/test/expected_results/", '45.b')
+const MODULE_42_PATH = path.resolve(__dirname + "/src/test/expected_results/", '42.b')
 const ADDITION_MODULE_MESSAGE_PATH = path.resolve(__dirname + "/src/test/expected_results/", 'additionModuleMessage.b')
 
 const testValue1 = 15
 const testValue2 = 30
-
+const testValue3 = 12
 
 testAddition()
 
 function testAddition() {
 
-  let additionCommand = eval.generateBrainfuckCommand(testValue1, testValue2, SUM_MODULE_PATH)
-  let testedValueCommand = fileReader.readFile(MODULE_45_PATH)
+  let additionCommand1 = eval.generateBrainfuckCommand(testValue1, testValue2, SUM_MODULE_PATH)
+  let additionCommand2 = eval.generateBrainfuckCommand(testValue2, testValue3, SUM_MODULE_PATH)
+  let testedValueCommand1 = fileReader.readFile(MODULE_45_PATH)
+  let testedValueCommand2 = fileReader.readFile(MODULE_42_PATH)
+
   let equalsModule = fileReader.readFile(EQUALS_MODULE)
 
-  let testCommand = additionCommand + testedValueCommand + equalsModule
-  const program1 = brainfuck(testCommand)
+  let testCommand1 = additionCommand1 + testedValueCommand1 + equalsModule
+  let testCommand2 = additionCommand2 + testedValueCommand2 + equalsModule
+
+  const program1 = brainfuck(testCommand1)
+  const program3 = brainfuck(testCommand2)
 
   program1.run()
+  program3.run()
 
   let testHeaderMessageCommand = fileReader.readFile(ADDITION_MODULE_MESSAGE_PATH)
   const program2 = brainfuck(testHeaderMessageCommand)
@@ -35,4 +43,5 @@ function testAddition() {
 
   console.log(program2.resultString())
   console.log(program1.resultString().slice(1))
+  console.log(program3.resultString().slice(1))
 }
